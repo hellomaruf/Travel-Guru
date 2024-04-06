b8bb8c767420332c196d1c716e2b6b661b400e94
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-06 16:19:42 +0600

    added navbar

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function Root() {
   return (
     <div>
-      <Nav />
+      
       <Outlet />
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,33 @@
+import Nav from "../../Shared/Nav";
 
 function Home() {
   return (
-    <div>
-      Home page
+    <div className="">
+      <div
+        className="min-h-screen bg-cover"
+        style={{
+          backgroundImage: "url('/images/hero.png')",
+        }}
+      >
+        <div className="bg-black min-h-screen bg-opacity-60">
+          <Nav />
+          <div className="flex items-center justify-center min-h-96  ">
+            <div className="">
+              <h1 className="text-white font-bebas font-bold text-5xl">
+                Cox's bazar
+              </h1>
+              <p className="text-white">
+                Cox's Bazar is a city, fishing port, tourism centre and district
+                headquarters in southeastern Bangladesh. It is famous mostly for
+                its long natural sandy beach, and it ...
+              </p>
+            </div>
+            <div className=" text-white">slider</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
