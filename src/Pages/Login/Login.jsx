ee96c35a80c5436d28ba3293646a2755b57bc138
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-07 17:16:03 +0600

    some problem solving

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,20 +2,38 @@ import { Link } from "react-router-dom";
 import NavTwo from "../../Shared/NavTwo";
 import { useContext } from "react";
 import { AuthContext } from "../../Services/AuthProvider";
-
+import { useForm } from "react-hook-form";
 function Login() {
-  const { name } = useContext(AuthContext);
-  
+  const {
+    register,
+    // handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm();
+
+  const { signIn } = useContext(AuthContext);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signIn(email, password)
+      .then((res) => console.log(res.user))
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <div className="bg-gray-100 h-screen">
       <NavTwo />
 
-      <form className="max-w-xl mx-auto  border-gray-700 p-6 shadow-md rounded-xl bg-white">
+      <form
+        onSubmit={handleLogin}
+        className="max-w-xl mx-auto  border-gray-700 p-6 shadow-md rounded-xl bg-white"
+      >
         <h2 className="text-2xl font-bold py-2">Login</h2>
         <div class="relative z-0 w-full mb-5 group">
           <input
-            type="email"
-            name="floating_email"
+            type="text"
+            name="name"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-amber-500 focus:outline-none focus:ring-0 focus:border-amber-600 peer"
             placeholder=" "
@@ -31,12 +49,13 @@ function Login() {
         <div class="relative z-0 w-full mb-5 group">
           <input
             type="email"
-            name="floating_email"
+            name="email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-amber-500 focus:outline-none focus:ring-0 focus:border-amber-600 peer"
             placeholder=" "
             required
           />
+
           <label
             for="floating_email"
             class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-amber-600 peer-focus:dark:text-amber-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -47,7 +66,7 @@ function Login() {
         <div className="relative z-0 w-full mb-5 group">
           <input
             type="password"
-            name="floating_password"
+            name="password"
             id="floating_password"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-amber-500 focus:outline-none focus:ring-0 focus:border-amber-600 peer"
             placeholder=" "
--- src/Services/AuthProvider.jsx
+++ src/Services/AuthProvider.jsx
@@ -1,13 +1,21 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { createContext } from "react";
 import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext();
 function AuthProvider({ children }) {
+  // create user
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
-  const authInfo = { createUser };
+  // sign in user
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const authInfo = { createUser, signIn };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
